456c3816082fd0e0fe07a2efa82ac1b6fa2d5b63
mdang8 <mdang808@gmail.com>
2017-08-18 15:40:32 -0400

    Auto stash for revert of "Check for Heroku port assignment"

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(__dirname));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000');
 });
 
