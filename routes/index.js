fa64bfbe2f1370f7c904ba7fc7ece645cebf3bde
mdang8 <mdang808@gmail.com>
2018-05-23 17:40:00 -0400

    Restructuring and created initial React components

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Is The Matrix On' });
-});
-
-module.exports = router;
---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './components/Main.jsx';
+
+import './scss/app.scss';
+
+render(<Main />, document.getElementById('root'));
