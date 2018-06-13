68b0f0202563bf48133dd3d5bb6c43d7b7a92549
mdang8 <mdang808@gmail.com>
2018-06-13 10:15:19 -0400

    Using .env file for environment variables

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const index = require('./routes/index');
 const users = require('./routes/users');
